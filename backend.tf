dda25394e59c8a475736b3a3e84ec2c2c75a48ac
Rakesh-7881 <rakesh7881kadam@gmail.com>
2025-09-16 16:40:38 +0530

    Update backend.tf
    update

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
   backend "azurerm" {
     resource_group_name   = "tfstate-rg"
-    storage_account_name  = "<your-storage-account-name>"
+    storage_account_name  = "tfstate69c67a8b"
     container_name        = "tfstate"
     key                   = "wordpress.tfstate"
   }
